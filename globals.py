b9d156cfdf37cfaee434e2c534331f001a7248b0
andreaforster <97340144+chicadelica@users.noreply.github.com>
2024-07-22 14:25:10 +0200

    Preprocessing_Master_Thesis

---
+++ globals.py
@@ -0,0 +1,3 @@
+YELP_DIR = "/Volumes/Forster Neu/Masterarbeit Data/yelp_dataset/"
+GOWALLA_DIR = "/Volumes/Forster Neu/Masterarbeit Data/gowalla_dataset/"
+BRIGHTKITE_DIR = "/Volumes/Forster Neu/Masterarbeit Data/brightkite_dataset/"
