12e6444f188f5fe117496054cedb62565864e7d7
andreaforster <97340144+chicadelica@users.noreply.github.com>
2024-09-15 23:51:14 +0200

    change paths

--- globals.py
+++ globals.py
@@ -1,3 +1,1 @@
-YELP_DIR = "/Volumes/Forster Neu/Masterarbeit Data/yelp_dataset/"
-GOWALLA_DIR = "/Volumes/Forster Neu/Masterarbeit Data/gowalla_dataset/"
-BRIGHTKITE_DIR = "/Volumes/Forster Neu/Masterarbeit Data/brightkite_dataset/"
+BASE_DIR = "/Volumes/Forster Neu/Masterarbeit Data/"
